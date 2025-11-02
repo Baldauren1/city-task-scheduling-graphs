48da45210d42549cedf790fd91b44ccf11424e33
Baldauren1 <baldaurenzamanov@gmail.com>
2025-11-02 12:11:10 +0500

    creating CondensationGraph

---
+++ src/main/java/graph/scc/CondensationGraph.java
@@ -0,0 +1,30 @@
+package graph.scc;
+
+import graph.common.Edge;
+import graph.common.Graph;
+import java.util.*;
+
+public class CondensationGraph {
+    public final Graph dag;
+    public final int[] compId;
+    public final int compCount;
+
+    public CondensationGraph(Graph g, List<List<Integer>> comps) {
+        compCount = comps.size();
+        dag = new Graph(compCount, true);
+        compId = new int[g.n];
+        for (int i = 0; i < compCount; i++) {
+            for (int v : comps.get(i)) compId[v] = i;
+        }
+
+        Set<Long> seen = new HashSet<>();
+        for (Edge e : g.edges) {
+            int cu = compId[e.u];
+            int cv = compId[e.v];
+            if (cu != cv) {
+                long key = (((long) cu) << 32) | (cv & 0xffffffffL);
+                if (seen.add(key)) dag.addEdge(cu, cv, e.w);
+            }
+        }
+    }
+}
